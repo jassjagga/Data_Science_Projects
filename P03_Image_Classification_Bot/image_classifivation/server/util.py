306b643d4bfcc40b80bd0da0da065a7cee0ca2f8
Jaswinder Singh <jaswindersinghjagga@gmail.com>
2022-10-18 08:08:29 -0400

    wnp

--- P03_Image_Classification_Bot/image_classifivation/server/util.py
+++ P03_Image_Classification_Bot/image_classifivation/server/util.py
@@ -53,15 +53,15 @@ def get_cropped_image_if_2_eyes(image_path,image_base64_data):
     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
     eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
     if image_path:
-        img=cv2.imread(image_path)
+        img = cv2.imread(image_path)
     else:
-        img= get_cv2_image_from_base64_string(image_base64_data)
+        img = get_cv2_image_from_base64_string(image_base64_data)
 
     if img is not None:
-        gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
         faces=face_cascade.detectMultiScale(gray,1.3,5)
 
-        cropped_faces=[]
+        cropped_faces = []
         for (x,y,w,h) in faces:
             roi_gray=gray[y:y+h,x:x+w]
             roi_color=img[y:y+h,x:x+w]
